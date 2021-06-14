e63afaf6b0c611a37f22d25e1dd7cc972accf15a
Amit Upadhyay <upadhyay_amit@yahoo.com>
2021-06-14 18:33:48 +0530

    Problem 5

--- src/main/java/com/collections/map/five/SolutionService.java
+++ src/main/java/com/collections/map/five/SolutionService.java
@@ -1,37 +1,34 @@
 package com.collections.map.five;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SolutionService {
 
-	private static Integer[] INPUT = { 1, 2, 3, 4, 5, 6, 7, 8, 9, -9, -10, 19 };
-	//private static Integer[] INPUT = { 6,6,6,6 };
+	//private static int[] INPUT = { 1, 2, 3, 4, 5, 6, 7, 8, 9, -9, -10, 19 }; // sum = 18
+	private static int[] INPUT = { 6,6,6,6 };
 	
-	private List<List<Integer>> result = new ArrayList<>();
+	private static List<List<Integer>> result;
 
-	private static Map<Integer, Integer> map = new HashMap<>();
+	private static Map<Integer, List<Pair>> map = new HashMap<>();
 
 	public static void main(String[] args) {
 		SolutionService service = new SolutionService();
+		Arrays.sort(INPUT);
 		service.createCountMap(INPUT);
-		service.getQuadrupletsForSum(INPUT, 18, 0, new ArrayList<>());
-		System.out.println(service.result);
+		result = new ArrayList<>();
+		service.getQuadrupletsForSum(INPUT, 24, 0, new ArrayList<>());
+		System.out.println(result);
+		result = new ArrayList<>();
+		service.getQuadrupletsForSumBetter(INPUT, 24);
+		System.out.println(result);
 	}
-
-	private void createCountMap(Integer[] input) {
-		for (Integer in : input) {
-			if (!map.containsKey(in)) {
-				map.put(in, 1);
-			} else {
-				map.put(in, map.get(in) + 1);
-			}
-		}
-	}
-
-	private void getQuadrupletsForSum(Integer[] input, int sum, int start, List<Integer> quadruplets) {
+	
+	private void getQuadrupletsForSum(int[] input, int sum, int start, List<Integer> quadruplets) {
 
 		int size = input.length;
 
@@ -40,7 +37,6 @@ public class SolutionService {
 			quadrupletsTemp.add(input[i]);
 			int diff = sum - input[i];
 			if (diff == 0 && quadrupletsTemp.size() == 4) {
-				System.out.println(quadrupletsTemp);
 				result.add(quadrupletsTemp);
 				continue;
 			} else if((diff == 0 && quadrupletsTemp.size() < 4)  || quadrupletsTemp.size() > 4) {
@@ -49,6 +45,67 @@ public class SolutionService {
 			getQuadrupletsForSum(input, diff, i + 1, quadrupletsTemp);
 		}
 		return;
+	}
+
+	private void createCountMap(int[] input) {
+		for (int i=0; i<input.length; i++) {
+			for (int j=i+1; j<input.length; j++) {
+                if(map.containsKey(input[i]+input[j])) {
+                    map.get(input[i]+input[j]).add(new Pair(i, j));
+                } else {
+                    List<Pair> list = new ArrayList<>();
+                    list.add(new Pair(i, j));
+				    map.put(input[i]+input[j], list);
+                }
+			}
+		}
+	}
+
+	private void getQuadrupletsForSumBetter(int[] input, int sum) {
+
+		int size = input.length;
+
+        Map<String, Integer> temp = new HashMap<>();
+        
+		for (int i = 0; i < size; i++) {
+			for (int j = i+1; j < size; j++) {
+				
+				Integer diff = sum - (input[i] + input[j]);
+				if (map.containsKey(diff)) {
+					List<Pair> lookups = map.get(diff);
+                    
+                    for (Pair lookup : lookups) {
+					    List<Integer> ans = new ArrayList<>();
+            
+                        if (lookup.x != i && lookup.x != j && lookup.y != i && lookup.y != j) {
+                            ans.add(input[i]);
+					        ans.add(input[j]);
+					        ans.add(input[lookup.x]);
+					        ans.add(input[lookup.y]);
+                            Collections.sort(ans);
+                            String key = ans.toString();
+                            if(!temp.containsKey(key)) {
+					            result.add(ans);
+                                temp.put(key, 1);
+                            }
+                      }
+                    }
+				}
+			}
+		}
+		return;
+	}
+	
+	private static class Pair {
+		
+		private int x;
+		
+		private int y;
+		
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 
 }
